76ac036d17ea33fb9fd05fd8f11a5430c3356da9
Marius Jonika <mariux255@gmail.com>
2020-11-05 18:37:54 +0100

    Working frame_classification

--- frame_classification_python.py
+++ frame_classification_python.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[26]:
 
 
 import tqdm
@@ -9,8 +9,11 @@ from preprocessing import exctract_json_data, define_categories
 import numpy as np
 import os
 import cv2
-
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import tqdm
 data_directory = "/Users/mjo/Desktop/WLASL/Processed_data/100"
 
 
@@ -39,7 +42,7 @@ def make_training_data(labels_x, video_id_dictionary, labels_iterated):
                     try:
                         path = os.path.join(data_directory,video, file)
                         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-                        training_data.append((np.array(img), np.eye(num_labels)[labels_iterated[label]]))
+                        training_data.append([np.array(img),labels_iterated[label]])
                     except Exception as e:
                         print(e)
                         pass
@@ -48,26 +51,49 @@ def make_training_data(labels_x, video_id_dictionary, labels_iterated):
 training_data = make_training_data(labels_100, video_id_dictionary, labels_iterated)
 
 
-# In[ ]:
+# In[27]:
 
 
-import torch
-trainloader = torch.utils.data.DataLoader(training_data, batch_size=20,
-                                          shuffle=True, num_workers=2)
+np.random.shuffle(training_data)
 
 
-# In[ ]:
+# In[28]:
+
+
+#import torch
+#trainloader = torch.utils.data.DataLoader(training_data, batch_size=20,
+#                                          shuffle=True, num_workers=2)
+X = [None] * len(training_data)
+y = [None] * len(X)
+counter = 0
+for i in training_data:
+    X[counter] = torch.Tensor(i[0])
+    y[counter] = i[1]
+    counter += 1
+
+
+# In[30]:
+
+
+training_inputs = torch.stack(X)
+training_labels = (np.array(y))
+
+
+# In[31]:
+
+
+training_labels = torch.LongTensor(y)
+
+
+# In[33]:
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
 
-# In[ ]:
+# In[34]:
 
-
-import torch.nn as nn
-import torch.nn.functional as F
 
 
 class Net(nn.Module):
@@ -82,56 +108,72 @@ class Net(nn.Module):
         self.fc3 = nn.Linear(16384, 100)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x.double())))
+        x = self.pool(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.pool2(F.relu(self.conv3(x)))
         x = F.relu(self.conv4(x))
-        x = x.view(20, -1)
+        x = x.view(-1, 16384)
         x = self.fc3(x)
-        return F.softmax(x, dim=1)
+        return x
 
 
-net = Net().double()
+net = Net()
 net = net.to(device)
 
 
-# In[ ]:
+# In[35]:
 
 
-import torch.optim as optim
-
-criterion = nn.MSELoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-
-# In[ ]:
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+criterion = criterion.to(device)
 
 
+# In[36]:
+
+
+def accuracy(ys, ts):
+    print("ts:", ts.shape)
+    print("ys:", ys.shape)
+    y = torch.argmax(ys, dim = 1)
+    x = torch.argmax(ts, dim = 1)
+    correct = 0
+    for i in range(len(y)):
+        if y[i] == x[i]:
+            correct += 1
+    return correct/len(y)
+
+
+# In[37]:
+
+
+BATCH_SIZE = 100
 for epoch in range(2):  # loop over the dataset multiple times
 
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
+    for i in range(0,len(training_inputs), BATCH_SIZE):
         # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+        inputs = training_inputs[i:i+BATCH_SIZE].view(-1, 1, 256, 256)
+        labels = training_labels[i:i+BATCH_SIZE]
+        #inputs, labels = batch
         inputs = inputs.to(device)
-        labels.to(device)
+        labels = labels.to(device)
         inputs = inputs.view(-1,1,256,256)
         # zero the parameter gradients
         optimizer.zero_grad()
         
         # forward + backward + optimize
+        
         outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        print(loss)
+        value, index = (torch.max(outputs,0))
+        value, index = (torch.max(labels,0))
+        #print(accuracy(outputs,labels))
+        preds = torch.max(outputs, 1)[1]
+        loss = criterion(outputs, torch.LongTensor(labels))
         loss.backward()
         optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+        print(f"Epoch: {epoch}. Loss: {loss}")
 
 print('Finished Training')
 
